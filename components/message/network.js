ec2c6ea74a0fcef62a50b5f44fa58ac23822fd3d
luis_parra <luis.parra.pulido@gmail.com>
2020-04-05 11:21:40 -0500

    Creación de entidades, relacion entre entidades, manejo de ficheros

--- components/message/network.js
+++ components/message/network.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const multer = require('multer');
 const response = require('../../network/response');
 const controller = require('./controller');
 const router = express.Router();
 
+const upload = multer ({
+  dest: 'public/files/',
+})
+
 router.get('/', function(req, res) {
-  controller.getMessages()
+  const filterMessages = req.query.chat || null;
+  controller.getMessages(filterMessages)
     .then((messageList) => {
       response.success(req, res, messageList, 200)
     })
@@ -12,9 +18,8 @@ router.get('/', function(req, res) {
       response.error(req, res, "Unexpected Error", 500, error)
     })
 })
-router.post('/', function(req, res) {
-  
-  controller.addMessage(req.body.user, req.body.message)
+router.post('/', upload.single('file'), function(req, res) {
+  controller.addMessage(req.body.chat, req.body.user, req.body.message, req.file)
     .then((fullmessage) => {
     response.success(req, res, fullmessage, 201);
     })
