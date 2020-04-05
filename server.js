68505f398922d1a43131a315eb1cc93fc237b05b
luis_parra <luis.parra.pulido@gmail.com>
2020-04-05 12:13:06 -0500

    utilizando websockets

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
+const server = require('http').Server(app);
 
+const bodyParser = require('body-parser');
+const socket = require('./socket')
 const db = require('./db');
+const router = require('./network/routes');
 
 const { config } = require('./config');
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const DB_NAME = config.dbName;
-
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
-
-const router = require('./network/routes');
-
 db(MONGO_URI);
 
-const app = express();
 
 app.use(bodyParser.json());
 
+socket.connect(server);
+
 router(app);
-
-
 
 app.use('/app', express.static('public'));
 
-app.listen(3000);
-console.log('La aplicación está escuchando en http://localhost:3000');
+server.listen(3000, function () {
+  console.log('La aplicación está escuchando en http://localhost:3000');
+});
